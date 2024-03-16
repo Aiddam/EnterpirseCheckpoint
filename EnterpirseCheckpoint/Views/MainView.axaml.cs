0fa666e45248d249ce9d69307c1eb7b1b7bd94de
Anton <sharlai.anton@booksoft.com.ua>
2024-03-16 11:55:21 +0200

    Add project files.

---
+++ EnterpirseCheckpoint/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace EnterpirseCheckpoint.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ EnterpirseCheckpoint/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace EnterpirseCheckpoint.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
