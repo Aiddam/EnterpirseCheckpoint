11ff2b48e57a48f092a2f2eab3083b2d678a89c4
Shut Kuva <zekazyza763@gmail.com>
2024-03-16 20:53:20 +0200

    Migrations

--- EnterpriseCheckpoint.Models/Models/Organization.cs
+++ EnterpriseCheckpoint.Models/Models/Organization.cs
@@ -7,7 +7,7 @@ namespace EnterpriseCheckpoint.Models.Models
         public string Name { get; set; } = string.Empty;
         public OrganizationType OrganizationType { get; set; }
         public string TaxCode { get; set; } = string.Empty;
-        public int PartnerId { get; set; }
-        public Partner Partner { get; set; } = null!;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
     }
 }
