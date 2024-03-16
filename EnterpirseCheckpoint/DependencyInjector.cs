9fef1a6b1f70db9c93c3fa6ead41737df7fe1954
Shut Kuva <zekazyza763@gmail.com>
2024-03-16 15:40:14 +0200

    Autofac dependency injection

---
+++ EnterpirseCheckpoint/DependencyInjector.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+
+namespace EnterpirseCheckpoint
+{
+    public static class DependencyInjector
+    {
+        public static void Load(ContainerBuilder containerBuilder)
+        {
+            containerBuilder
+                .RegisterAssemblyTypes(typeof(DependencyInjector).Assembly)
+                .Where(t => 
+                    t.Name.Contains("View") ||
+                    t.Name.Contains("Window")
+                )
+                .AsSelf()
+                .AsImplementedInterfaces();
+
+            EnterpriseCheckpoint.Services.DependencyInjector.Load(containerBuilder);
+            Utilities.DependencyInjector.Load(containerBuilder);
+        }
+    }
+}
